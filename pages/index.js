c7bcbb0bb6843c334b18e0f916229d4f6695768a
RamtinNorouziiii <rn.dejavoo@gmail.com>
2022-07-20 14:50:17 +0430

    Enter Button Registerd

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import Styles from "../styles/search.module.scss";
 export default function Home() {
   const [username, setUsername] = useState("");
-  const [user, setUser] = useState();
   const[notFound,setNotFound]=useState(false)
   const InputHandler = (e) => {
     setUsername(e.target.value);
   };
-  const startSearch = async () => {
+  const startSearch = async (e) => {
+    e.preventDefault()
     const res = await fetch(`https://api.github.com/users/${username}`).then((res)=>{
      return  res
     }).catch((err)=>{return err})
@@ -20,10 +20,12 @@ export default function Home() {
     <div className=" text-center">
       <div className={Styles.searchbarHolder}>
         <label>Search Github Username :</label>{" "}
+        <form>
         <input className="mx-2" onChange={InputHandler} value={username} />
-        <button onClick={startSearch} className="btn btn-success">
+        <button type="submit" onClick={startSearch} className="btn btn-success">
           Search
         </button>
+        </form>
         <h3 className="text-danger mt-5" >{notFound ?"User Not Found" :""}</h3>
       </div>
     </div>
